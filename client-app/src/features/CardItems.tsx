b2bd4bde673db3046d1ab42f7c327426e258187a
NourhatHasan <https://github.com/NourhatHasan>
2023-06-27 21:05:06 +0200

    done with deleting from the shoping bag

--- client-app/src/features/CardItems.tsx
+++ client-app/src/features/CardItems.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { observer } from "mobx-react-lite";
-import { Button, Icon, Input, List, ListItem } from "semantic-ui-react";
+import { Button, Icon, List} from "semantic-ui-react";
 import { useStore } from "../layout/Stores/Store";
 
 export default observer(function CardItems() {
@@ -25,23 +25,29 @@ export default observer(function CardItems() {
   
 
     return (
-        <div style={{ marginTop: '6em' }}>
-            <List divided>
+        <div className="card-items-container">
+            <List divided relaxed>
                 {shopingStore.CardItems.map((movie) => (
                     <List.Item key={movie.movieId}>
-                        <List.Content floated="left">{movie.movieName}</List.Content>
+                        <List.Content floated="left">
+                            {movie.movieName}
+                        </List.Content>
                         <List.Content floated="right">
                             <Button icon onClick={() => handleDecreaseAmount(movie.movieId)}>
                                 <Icon name="minus" />
                             </Button>
 
-                            <span style={{ marginRight: '8px', marginLeft: '8px' }}>{movie.mengde}</span>
+                            <span className="item-quantity">{movie.mengde}</span>
 
                             <Button icon onClick={() => handleIncreaseAmount(movie.movieId)}>
                                 <Icon name="plus" />
                             </Button>
 
-                            <Button icon color="red" onClick={() => handleDelete(movie.movieId)}>
+                            <Button
+                                icon
+                                color="red"
+                                onClick={() => handleDelete(movie.movieId)}
+                            >
                                 <Icon name="trash" />
                             </Button>
                         </List.Content>
